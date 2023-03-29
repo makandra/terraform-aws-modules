01a9eceab69009575ba9bcab4932b01f16dd537c
Claus-Theodor Riegg <claus.riegg@makandra.de>
2023-03-29 16:01:57 +0200

    fix(alternate_contacts): refactoring and removal of unneeded datasource

--- modules/alternate_contacts/data.tf
+++ modules/alternate_contacts/data.tf
@@ -1,7 +1,3 @@
 data "aws_organizations_organization" "this" {
   count = var.assign_contacts_to_all_accounts ? 1 : 0
 }
-
-data "aws_caller_identity" "current" {
-  count = var.assign_contacts_to_all_accounts ? 0 : 1
-}
