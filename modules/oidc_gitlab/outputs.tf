a95691dde8e9747409a6a907e39ba4317052a5a7
Ruben Aleman <ruben.aleman@makandra.de>
2025-06-26 13:58:05 +0200

    feat: add oidc_connect_provider_config as output for oidc_gitlab

--- modules/oidc_gitlab/outputs.tf
+++ modules/oidc_gitlab/outputs.tf
@@ -2,3 +2,8 @@ output "gitlab_role_arn" {
   description = "Role that needs to be assumed by GitLab CI."
   value       = aws_iam_role.this.arn
 }
+
+output "oidc_connect_provider_config" {
+  description = "Configuration of the aws_iam_openid_connect_provider."
+  value       = aws_iam_openid_connect_provider.this
+}
